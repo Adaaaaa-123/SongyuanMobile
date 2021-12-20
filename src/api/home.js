b1cfaf72573eccdf6b4d84edf16123d97dbc950d
zhoubaiqiang <1121915087@qq.com>
2021-12-20 15:30:28 +0800

    签收和误报接口联调 增加openlayers组件

--- src/api/home.js
+++ src/api/home.js
@@ -11,12 +11,24 @@ export const getList = params => {
   });
 }
 
-// 采纳修改
-export const disposeEarly = params => {
+
+// 误报
+export const distort = params => {
   return request({
-    url: `/earlyWarning/disposeEarly/${params.id}`,
+    url: `/blbWarnCarRelation/distort/${params.id}`,
+    method: 'post',
+    responseType: 'json',
+    data:params
+  });
+}
+
+
+// 签收
+export const receipt = params => { 
+  return request({
+    url: `/blbWarnCarRelation/receipt/${params.id}`,
     method: 'get',
     responseType: 'json',
-    params
+    params:{}
   });
 }
